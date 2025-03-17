20b1cac1642555003679d3a8426e89d8ccbae16d
Mireya <vellamireya@gmail.com>
2025-03-17 21:38:25 +0100

    fixed base in config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,8 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -19,10 +15,14 @@ export default defineConfig({
       '/api': {
         target: 'https://diy-project-organizer-default-rtdb.europe-west1.firebasedatabase.app',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // Removes "/api" from the URL before making the request
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
-  // Set the base path for GitHub Pages deployment
+  // Ensure GitHub Pages loads assets correctly
   base: process.env.NODE_ENV === 'production' ? '/Project-Organiser/' : '/',
+  build: {
+    assetsDir: 'assets',
+    outDir: 'dist', // Ensure the output directory is correct
+  },
 });
